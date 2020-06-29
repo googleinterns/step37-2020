b80747e552e5ecd7b5bd5f990c616daa8803290c
Peter Geertsema <peterjgeertsema@gmail.com>
2020-06-29 21:38:06 +0000

    Add test for faking request()

--- src/test/javascript/utils.spec.ts
+++ src/test/javascript/utils.spec.ts
@@ -1,24 +1,33 @@
 import { strictEqual } from 'assert';
-import { fallOnSameDay } from '../../main/webapp/utils';
+import * as utils from '../../main/webapp/utils';
 
 describe('Utility functions', () => {
-  describe('fallOnSameDay', () => {
+  describe('fallOnSameDay()', () => {
     it('Should work for equal times', () => {
       let date1 = new Date(2020, 6, 1);
       let date2 = new Date(2020, 6, 1);
-      strictEqual(fallOnSameDay(date1.getTime(), date2.getTime()), true);
+      strictEqual(utils.fallOnSameDay(date1.getTime(), date2.getTime()), true);
     });
 
     it('Should work for different hours', () => {
       let date1 = new Date(2020, 6, 1, 7);
       let date2 = new Date(2020, 6, 1, 12);
-      strictEqual(fallOnSameDay(date1.getTime(), date2.getTime()), true);
+      strictEqual(utils.fallOnSameDay(date1.getTime(), date2.getTime()), true);
     });
 
     it('Should detect exact 24 hour difference', () => {
       let date1 = new Date(2020, 6, 1);
       let date2 = new Date(2020, 1, 7);
-      strictEqual(fallOnSameDay(date1.getTime(), date2.getTime()), false);
+      strictEqual(utils.fallOnSameDay(date1.getTime(), date2.getTime()), false);
+    });
+  });
+
+  describe('request()', () => {
+    it('Should fake out correctly', async () => {
+      let fake = { value: false, integer: 7 };
+      let url = '/faked';
+      utils.setResponse(url, fake);
+      strictEqual(await utils.request(url, 'GET', undefined, true).then(r => r.json()), fake)
     });
   });
 });

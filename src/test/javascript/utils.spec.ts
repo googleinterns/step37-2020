289723a0a3575893a1100b94a9bed529c7459322
Peter Geertsema <peterjgeertsema@gmail.com>
2020-06-29 21:21:14 +0000

    Add some tests

---
+++ src/test/javascript/utils.spec.ts
@@ -0,0 +1,24 @@
+import { strictEqual } from 'assert';
+import { fallOnSameDay } from '../../main/webapp/utils';
+
+describe('Utility functions', () => {
+  describe('fallOnSameDay', () => {
+    it('Should work for equal times', () => {
+      let date1 = new Date(2020, 6, 1);
+      let date2 = new Date(2020, 6, 1);
+      strictEqual(fallOnSameDay(date1.getTime(), date2.getTime()), true);
+    });
+
+    it('Should work for different hours', () => {
+      let date1 = new Date(2020, 6, 1, 7);
+      let date2 = new Date(2020, 6, 1, 12);
+      strictEqual(fallOnSameDay(date1.getTime(), date2.getTime()), true);
+    });
+
+    it('Should detect exact 24 hour difference', () => {
+      let date1 = new Date(2020, 6, 1);
+      let date2 = new Date(2020, 1, 7);
+      strictEqual(fallOnSameDay(date1.getTime(), date2.getTime()), false);
+    });
+  });
+});

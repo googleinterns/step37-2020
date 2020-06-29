a6b10e83360b78a097c2c7e9d8ab5491c611105a
Ionis <ionis@google.com>
2020-06-29 21:28:22 +0000

    Commented servlet functionality and added API utilities RecommendationRetriever.java

---
+++ src/main/java/com/google/impactdashboard/api_utilities/RecommendationRetriever.java
@@ -0,0 +1,11 @@
+package com.google.impactdashboard.api_utilities;
+
+import com.google.api.services.recommender.v1.Recommender;
+import com.google.api.services.recommender.v1.Recommender.Projects.Locations.Recommenders.Recommendations;
+
+/**
+ * Class that will call the Recommender API to get the full information for recommendations
+ */
+public class RecommendationRetriever {
+  // TODO: find out how to initialize recommender to send ListRecommendationsrequest
+}

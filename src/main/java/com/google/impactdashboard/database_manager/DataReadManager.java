5b4dcdd903394ab00abba29c83a296b76a9fe079
Caroline Lui <cel243@cornell.edu>
2020-06-30 14:21:36 +0000

    Add interfaces for Database Manager

---
+++ src/main/java/com/google/impactdashboard/database_manager/DataReadManager.java
@@ -0,0 +1,36 @@
+package com.google.impactdashboard.database_manager;
+
+import com.google.impactdashboard.data.project.ProjectIdentification;
+import com.google.impactdashboard.data.recommendation.Recommendation;
+import java.util.List;
+import java.util.Map;
+
+/** Interface for retrieving data from the database. */
+public interface DataReadManager {
+  /** 
+   *  Returns a list containing identifying information for all projects in the 
+   *  IAM Bindings table.  
+   */
+  public List<ProjectIdentification> listProjects();
+
+  /** 
+   *  Returns the average number of IAM bindings that the project with id 
+   *  {@code projectId} had per day over the past 365 days (or, if there are 
+   *  not 365 days of data in the IAM Bindings table, the average over however 
+   *  many days of data are in the table). 
+   */
+  public double getAverageIAMBindingsInPastYear(String projectId);
+
+  /**
+   *  Returns a map of dates (as timestamps in UTC milliseconds since the epoch) 
+   *  to the Recommendation applied on that date to the project with id {@code projectId}.
+   */
+  public Map<Long, Recommendation> getMapOfDatesToRecommendationTaken(String projectId);
+
+  /** 
+   *  Returns a map of dates (as timestamps in UTC milliseconds since the epoch) 
+   *  to the number of IAM bindings that existed for the project with id {@code projectId} 
+   *  on that date.
+   */
+  public Map<Long, Integer> getMapOfDatesToIAMBindings(String projectId);
+}
---
+++ src/main/java/com/google/impactdashboard/database_manager/DataUpdateManager.java
@@ -0,0 +1,20 @@
+package com.google.impactdashboard.database_manager;
+
+import com.google.impactdashboard.data.recommendation.Recommendation;
+import com.google.impactdashboard.data.IAMBindingDatabaseEntry;
+import java.util.List;
+
+/** Interface for managing updates to the database. */
+public interface DataUpdateManager {
+  /** 
+   *  Deletes data from both the Recommendations and IAM Bindings tables if that 
+   *  data is over 365 days old.  
+   */
+  public void deleteYearOldData();
+
+  /** Stores inputted recommendations in the Recommendations table */
+  public void updateRecommendations(List<Recommendation> recommendations);
+
+  /** Stores inputted IAM bindings information in the IAM Bindings table. */
+  public void updateIAMBindings(List<IAMBindingDatabaseEntry> iamBindingsData);
+}

80f622d68e7e4580ca52befa0972bd61a2c0c451
Ionis <ionis@google.com>
2020-07-01 13:28:07 +0000

    Fix style issues

--- src/main/java/com/google/impactdashboard/api_utilities/LogRetriever.java
+++ src/main/java/com/google/impactdashboard/api_utilities/LogRetriever.java
@@ -2,10 +2,10 @@ package com.google.impactdashboard.api_utilities;
 
 import com.google.cloud.logging.v2.LoggingClient;
 import com.google.logging.v2.LogEntry;
-
 import java.io.IOException;
 import java.util.Collection;
 
+/** Class that handles all the retrieval of logs stored on the cloud logging API. */
 public class LogRetriever {
 
   /** Cloud Logging client used to retrieve Audit logs and Recommendation logs */
@@ -16,7 +16,7 @@ public class LogRetriever {
   }
 
   /**
-   * Function used to create a ListLogEntriesRequest and retrieve all the relevant audit logs
+   * Function used to create a {@code ListLogEntriesRequest} and retrieve all the relevant audit logs
    * @return A list of all the relevant audit log entries that are stored by the logging API
    */
   public Collection<LogEntry> listAuditLogs(String[] resourceNames, String pageToken) {
@@ -24,7 +24,7 @@ public class LogRetriever {
   }
 
   /**
-   * Function used to create create a ListLogEntriesRequest and retrieve all the relevant Recommendation logs
+   * Function used to create create a {@code ListLogEntriesRequest} and retrieve all the relevant Recommendation logs
    * @return A list of all the relevant recommendation log entries that are stored by the logging API.
    */
   public Collection<LogEntry> listRecommendationLogs(String[] resourceNames, String pageToken) {

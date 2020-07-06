df0e84b1dbeb9e9f53c4399e2a0a2036a5db68b4
Caroline Lui <cel243@cornell.edu>
2020-07-06 13:14:17 +0000

    Replace parseLong with L

--- src/main/java/com/google/impactdashboard/database_manager/FakeDatabase.java
+++ src/main/java/com/google/impactdashboard/database_manager/FakeDatabase.java
@@ -14,7 +14,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 /** A class that maintains a fake database for testing purposes. */
 public class FakeDatabase {
-  private static final long MILLISECONDS_365_DAYS = Long.parseLong("31536000000");
+  private static final long MILLISECONDS_365_DAYS = 31536000000L;
   private static final long MILLISECONDS_ONE_DAY = 86400000;
 
   /** 
@@ -26,30 +26,30 @@ public class FakeDatabase {
         private static final long serialVersionUID = 1L;
 
         {
-        put(Long.parseLong("1592486705000"), 
+        put(1592486705000L, 
           Recommendation.create("project-id-1", 
             "remove unused permissions from user account test3@google.com", 
-            Recommendation.RecommenderType.IAM_BINDING, Long.parseLong("1592486705000"), 
+            Recommendation.RecommenderType.IAM_BINDING, 1592486705000L, 
             IAMRecommenderMetadata.create(300)));
-        put(Long.parseLong("1592486585000"), 
+        put(1592486585000L, 
           Recommendation.create("project-id-1", 
             "remove unused permissions from user account test2@google.com", 
-            Recommendation.RecommenderType.IAM_BINDING, Long.parseLong("1592486585000"), 
+            Recommendation.RecommenderType.IAM_BINDING, 1592486585000L, 
             IAMRecommenderMetadata.create(1000))); 
-        put(Long.parseLong("1591633823000"), 
+        put(1591633823000L, 
           Recommendation.create("project-id-1", 
             "remove unused permissions from user account test@google.com", 
-            Recommendation.RecommenderType.IAM_BINDING, Long.parseLong("1591633823000"), 
+            Recommendation.RecommenderType.IAM_BINDING, 1591633823000L, 
             IAMRecommenderMetadata.create(1000))); 
-        put(Long.parseLong("1591704613000"), 
+        put(1591704613000L, 
           Recommendation.create("project-id-2", 
             "remove unused permissions from user account test4@google.com", 
-            Recommendation.RecommenderType.IAM_BINDING, Long.parseLong("1591704613000"), 
+            Recommendation.RecommenderType.IAM_BINDING, 1591704613000L, 
             IAMRecommenderMetadata.create(500)));  
-        put(Long.parseLong("1593072312000"), 
+        put(1593072312000L, 
           Recommendation.create("project-id-2", 
             "remove unused permissions from user account test5@google.com", 
-            Recommendation.RecommenderType.IAM_BINDING, Long.parseLong("1593072312000"), 
+            Recommendation.RecommenderType.IAM_BINDING, 1593072312000L, 
             IAMRecommenderMetadata.create(350)));    
       }
     };
@@ -61,7 +61,7 @@ public class FakeDatabase {
 
         {
         HashMap<Long, Integer> bindingsProject1 = new HashMap<Long, Integer>();
-        AtomicReference<Long> date1 = new AtomicReference<Long>(Long.parseLong("1590883200000"));
+        AtomicReference<Long> date1 = new AtomicReference<Long>(1590883200000L);
 
         Arrays.asList(1000, 1000, 1000, 2000, 2050, 2150, 2150, 2150, 2150, 
         1150, 1150, 1150, 1150, 2000, 2000, 2500, 2500, 2300, 2300, 1000, 
@@ -72,10 +72,10 @@ public class FakeDatabase {
         });
 
         put(ProjectIdentification.create("project-1", "project-id-1", 
-          Long.parseLong("123456789123")), bindingsProject1);
+          123456789123L), bindingsProject1);
 
         HashMap<Long, Integer> bindingsProject2 = new HashMap<Long, Integer>();
-        AtomicReference<Long> date2 = new AtomicReference<Long>(Long.parseLong("1590883200000"));
+        AtomicReference<Long> date2 = new AtomicReference<Long>(1590883200000L);
 
         Arrays.asList(500, 500, 750, 750, 750, 750, 750, 1000, 1000, 
         1000, 500, 500, 500, 600, 600, 600, 600, 300, 300, 1000, 
@@ -86,7 +86,7 @@ public class FakeDatabase {
         });
 
         put(ProjectIdentification.create("project-2", "project-id-2", 
-          Long.parseLong("234567890123")), bindingsProject2);
+          234567890123L), bindingsProject2);
       }
   };
 

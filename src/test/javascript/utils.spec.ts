50d28a2729396fce9b280eb18ceb3299f7bb1ed8
Peter Geertsema <peterjgeertsema@gmail.com>
2020-07-06 17:58:43 +0000

    Add tests for removeFromGraph()

--- src/test/javascript/utils.spec.ts
+++ src/test/javascript/utils.spec.ts
@@ -189,7 +189,7 @@ describe('Utility functions', () => {
 
       strictEqual(properties.options.series[0].color, 'white');
       strictEqual(properties.columns.length, 4);
-      for(let i = 0; i < 3; i++) {
+      for (let i = 0; i < 3; i++) {
         strictEqual(properties.graphData[i][0].getTime(), dates[i].getTime());
       }
     });
@@ -201,16 +201,74 @@ describe('Utility functions', () => {
       strictEqual(properties.options.series[0].color, 'white');
       strictEqual(properties.options.series[1].color, 'black');
       strictEqual(properties.columns.length, 7);
-      for(let i = 0; i < 6; i++) {
+      for (let i = 0; i < 6; i++) {
         strictEqual(properties.graphData[i][0].getTime(), dates[i].getTime());
         // Make sure there's no overlap in values since projects are on two different time ranges
-        if(i < 3) {
+        if (i < 3) {
           strictEqual(properties.graphData[i][4], undefined);
           strictEqual(properties.graphData[i][5], undefined);
         } else {
           strictEqual(properties.graphData[i][1], undefined);
           strictEqual(properties.graphData[i][2], undefined);
         }
+      }
+    });
+  });
+
+  describe('removeFromGraph()', () => {
+    let properties: { options: google.visualization.LineChartOptions, graphData: any[], columns: any[], };
+    let prj1: Project, prj1Data: ProjectGraphData;
+    let prj2: Project, prj2Data: ProjectGraphData;
+    let dates: Date[];
+    beforeEach(() => {
+      dates = [new Date(2020, 6, 1), new Date(2020, 6, 2), new Date(2020, 6, 3), new Date(2020, 7, 1), new Date(2020, 7, 2), new Date(2020, 7, 3)];
+      let dateToIamBindings = {
+        [dates[0].getTime()]: 100,
+        [dates[1].getTime()]: 150,
+        [dates[2].getTime()]: 200
+      };
+
+      properties = utils.initProperties();
+      prj1 = new Project('1', '1', 1, new ProjectMetaData(100));
+      prj1.color = 'white';
+      prj1Data = new ProjectGraphData('1', dateToIamBindings, {});
+
+      dateToIamBindings = {
+        [dates[3].getTime()]: 70,
+        [dates[4].getTime()]: 75,
+        [dates[5].getTime()]: 85
+      }
+      prj2 = new Project('2', '2', 2, new ProjectMetaData(150));
+      prj2.color = 'black';
+      prj2Data = new ProjectGraphData('2', dateToIamBindings, {});
+
+      utils.addToGraph(properties, prj1Data, prj1);
+      utils.addToGraph(properties, prj2Data, prj2);
+    });
+
+    it('Should be able to remove a single project', () => {
+      utils.removeFromGraph(properties, prj1);
+
+      strictEqual(properties.columns.length, 4);
+      strictEqual(properties.options.series[0].color, 'black');
+      // Should leave dates intact but be empty
+      for (let i = 0; i < 6; i++) {
+        strictEqual(properties.graphData[i][0].getTime(), dates[i].getTime());
+        if (i < 3) {
+          strictEqual(properties.graphData[i][1], undefined);
+          strictEqual(properties.graphData[i][2], undefined);
+        }
+      }
+    });
+
+    it('Should remove both projects', () => {
+      utils.removeFromGraph(properties, prj1);
+      utils.removeFromGraph(properties, prj2);
+
+      strictEqual(properties.columns.length, 1);
+      // Should leave dates intact but be empty
+      for (let i = 0; i < 6; i++) {
+        strictEqual(properties.graphData[i][0].getTime(), dates[i].getTime());
       }
     });
   });

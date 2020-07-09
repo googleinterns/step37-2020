1d38ddb2eddc864a6eaf2bfccbb6f0f5574a3c17
Caroline Lui <cel243@cornell.edu>
2020-07-09 18:32:56 +0000

    Add newline

--- src/main/java/com/google/impactdashboard/configuration/Constants.java
+++ src/main/java/com/google/impactdashboard/configuration/Constants.java
@@ -20,5 +20,5 @@ public class Constants {
   public static final String TEST_DATABASE = "Test_Capstone";
 
   /** The name of the database holding the real data. */
-  public static final String DATABASE = "";
+  public static final String DATABASE = "Placeholder";
 }

daca747f5d7a4035437d3c7af509400661fa2d3c
Caroline Lui <cel243@cornell.edu>
2020-07-09 19:31:58 +0000

    Write test suite

---
+++ src/test/java/com/google/impactdashboard/server/DataReadManagerTest.java
@@ -0,0 +1,147 @@
+package com.google.impactdashboard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import com.google.impactdashboard.configuration.Configuration;
+import com.google.impactdashboard.data.project.ProjectIdentification;
+import com.google.impactdashboard.data.recommendation.*;
+import com.google.impactdashboard.database_manager.data_read.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.io.IOException;
+
+@RunWith(JUnit4.class)
+public class DataReadManagerTest {
+
+  private static DataReadManager dataReadManager;
+
+  private static final ProjectIdentification PROJECT_1_IDENTIFICATION = 
+    ProjectIdentification.create("project-1", "project-id-1", 123456789123L);
+  private static final ProjectIdentification PROJECT_2_IDENTIFICATION = 
+    ProjectIdentification.create("project-2", "project-id-2", 234567890123L);
+  private static final String PROJECT_ID_1 = "project-id-1";
+  private static final String PROJECT_ID_2 = "project-id-2";
+  private static final Recommendation PROJECT_2_RECOMMENDATION_ON_25TH = 
+    Recommendation.create(
+      PROJECT_ID_2, "Remove unused permissions from user test5@example.com", 
+      Recommendation.RecommenderType.IAM_BINDING, 1593115512000L, 
+      IAMRecommenderMetadata.create(350));
+  private static final Recommendation PROJECT_2_RECOMMENDATION_ON_9TH = 
+    Recommendation.create(
+      PROJECT_ID_2, "Remove unused permissions from user test4@example.com", 
+      Recommendation.RecommenderType.IAM_BINDING, 1591704613000L, 
+      IAMRecommenderMetadata.create(500));
+
+  @BeforeClass
+  public static void setTestingConfiguration() throws IOException {
+    Configuration.useTestDatabase = true;
+    dataReadManager = DataReadManagerFactory.create();
+  }
+
+  @AfterClass
+  public static void undoTestingConfiguration() {
+    Configuration.useTestDatabase = false;
+  }
+
+  @Test
+  public void testAllProjectsListedCorrectly() {
+    List<ProjectIdentification> actual = dataReadManager.listProjects();
+    List<ProjectIdentification> expected = Arrays.asList(
+      PROJECT_1_IDENTIFICATION, PROJECT_2_IDENTIFICATION);
+
+    assertEquals(expected.size(), actual.size());
+    actual.removeAll(expected);
+    assertEquals(Arrays.asList(), actual);
+  }
+
+  @Test
+  public void testAverageBindingsOnProject1() {
+    double actual = dataReadManager.getAverageIAMBindingsInPastYear(PROJECT_ID_1);
+    double expected = 1545;
+
+    assertEquals(expected, actual, 0.001);
+  }
+
+  @Test
+  public void testAverageBindingsOnProject2() {
+    double actual = dataReadManager.getAverageIAMBindingsInPastYear(PROJECT_ID_2);
+    double expected = 715;
+
+    assertEquals(expected, actual, 0.001);
+  }
+
+  @Test
+  public void testAverageBindingsReturnsZeroForNonexistentProject() {
+    double actual = dataReadManager.getAverageIAMBindingsInPastYear("does-not-exist");
+    double expected = 0;
+
+    assertEquals(expected, actual, 0.001);
+  }
+
+  @Test
+  public void testCorrectNumberOfDatesToBindingsReturnedOnProject1() {
+    int actual = dataReadManager.getMapOfDatesToIAMBindings(PROJECT_ID_1).size();
+    int expected = 30;
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testCorrectNumberOfDatesToRecommendationsReturnedOnProject1() {
+    int actual = dataReadManager.getMapOfDatesToRecommendationTaken(PROJECT_ID_1).size();
+    int expected = 3;
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testRandomDatesToBindingsForProject2() {
+    Map<Long, Integer> datesToBindings = dataReadManager.getMapOfDatesToIAMBindings(PROJECT_ID_2);
+
+    assertTrue(datesToBindings.containsKey(1591963200000L));
+    assertEquals((Integer) 500, datesToBindings.get(1591963200000L));
+
+    assertTrue(datesToBindings.containsKey(1593086400000L));
+    assertEquals((Integer) 1000, datesToBindings.get(1593086400000L));
+  }
+
+  @Test
+  public void testContentsOfDatesToRecommendationsForProject2() {
+    Map<Long, Recommendation> datesToRecommendations = 
+      dataReadManager.getMapOfDatesToRecommendationTaken(PROJECT_ID_2);  
+
+    assertTrue("Timestamp of recommendation on 25th is retrieved from the database.", 
+      datesToRecommendations.containsKey(
+        PROJECT_2_RECOMMENDATION_ON_25TH.getAcceptedTimestamp()));
+    assertEquals(PROJECT_2_RECOMMENDATION_ON_25TH, 
+      datesToRecommendations.get(PROJECT_2_RECOMMENDATION_ON_25TH.getAcceptedTimestamp()));
+
+    assertTrue("Timestamp of recommendation on 9th is retrieved from the database.", 
+      datesToRecommendations.containsKey(
+        PROJECT_2_RECOMMENDATION_ON_9TH.getAcceptedTimestamp()));
+    assertEquals(PROJECT_2_RECOMMENDATION_ON_9TH, 
+      datesToRecommendations.get(PROJECT_2_RECOMMENDATION_ON_9TH.getAcceptedTimestamp()));
+  }
+
+  @Test
+  public void testNoRecommendationsReturnedForNonexistentProject() {
+    int actual = dataReadManager.getMapOfDatesToRecommendationTaken("does-not-exist").size();
+    int expected = 0;
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testNoBindingsReturnedForNonexistentProject() {
+    int actual = dataReadManager.getMapOfDatesToIAMBindings("does-not-exist").size();
+    int expected = 0;
+
+    assertEquals(expected, actual);
+  } 
+}
---
+++ src/test/java/com/google/impactdashboard/server/DataUpdateManagerTest.java
@@ -0,0 +1,136 @@
+package com.google.impactdashboard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.runners.MethodSorters;
+import com.google.impactdashboard.configuration.Configuration;
+import com.google.impactdashboard.data.project.ProjectIdentification;
+import com.google.impactdashboard.data.recommendation.*;
+import com.google.impactdashboard.data.IAMBindingDatabaseEntry;
+import com.google.impactdashboard.database_manager.data_read.*;
+import com.google.impactdashboard.database_manager.data_update.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.io.IOException;
+
+@RunWith(JUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DataUpdateManagerTest {
+
+  private static DataReadManager dataReadManager;
+  private static DataUpdateManager dataUpdateManager;
+
+  private static final ProjectIdentification PROJECT_2_IDENTIFICATION = 
+    ProjectIdentification.create("project-2", "project-id-2", 234567890123L);
+  private static final String PROJECT_ID_1 = "project-id-1";
+  private static final String PROJECT_ID_2 = "project-id-2";
+  private static final Recommendation PROJECT_1_RECOMMENDATION_ON_20190628 = 
+    Recommendation.create(
+      PROJECT_ID_1, "Remove unused permissions", 
+      Recommendation.RecommenderType.IAM_BINDING, 1561723200000L, 
+      IAMRecommenderMetadata.create(100));
+  private static final Recommendation PROJECT_1_RECOMMENDATION_ON_20190625 = 
+    Recommendation.create(
+      PROJECT_ID_1, "Remove unused permissions", 
+      Recommendation.RecommenderType.IAM_BINDING, 1561464000000L, 
+      IAMRecommenderMetadata.create(500));
+  private static final IAMBindingDatabaseEntry PROJECT_2_BINDINGS_DATA_ON_20190523 = 
+    IAMBindingDatabaseEntry.create(
+      PROJECT_ID_2, PROJECT_2_IDENTIFICATION.getName(), 
+      PROJECT_2_IDENTIFICATION.getProjectNumber()+"", 1558612800000L, 2000);
+  private static final IAMBindingDatabaseEntry PROJECT_2_BINDINGS_DATA_ON_20190620 = 
+    IAMBindingDatabaseEntry.create(
+      PROJECT_ID_2, PROJECT_2_IDENTIFICATION.getName(), 
+      PROJECT_2_IDENTIFICATION.getProjectNumber()+"", 1561032000000L, 1000);
+
+  @BeforeClass
+  public static void setTestingConfiguration() throws IOException {
+    Configuration.useTestDatabase = true;
+    dataReadManager = DataReadManagerFactory.create();
+    dataUpdateManager = DataUpdateManagerFactory.create();
+  }
+
+  @AfterClass
+  public static void undoTestingConfiguration() {
+    Configuration.useTestDatabase = false;
+  }
+
+  @Test
+  public void testARecommendationsAddedToDatabase() {
+    dataUpdateManager.updateRecommendations(Arrays.asList(
+      PROJECT_1_RECOMMENDATION_ON_20190628, PROJECT_1_RECOMMENDATION_ON_20190625));
+    Map<Long, Recommendation> datesToRecommendations = dataReadManager
+      .getMapOfDatesToRecommendationTaken(PROJECT_ID_1);
+
+    assertTrue("The database now contains the dates that were just added",
+      datesToRecommendations.containsKey(PROJECT_1_RECOMMENDATION_ON_20190625
+        .getAcceptedTimestamp()) && 
+      datesToRecommendations.containsKey(PROJECT_1_RECOMMENDATION_ON_20190628
+        .getAcceptedTimestamp()));
+    assertEquals(
+      PROJECT_1_RECOMMENDATION_ON_20190625, datesToRecommendations
+        .get(PROJECT_1_RECOMMENDATION_ON_20190625.getAcceptedTimestamp()));
+    assertEquals(
+      PROJECT_1_RECOMMENDATION_ON_20190628, datesToRecommendations
+        .get(PROJECT_1_RECOMMENDATION_ON_20190628.getAcceptedTimestamp()));
+  }
+
+  @Test
+  public void testBIAMBindingsAddedToDatabase() {
+    dataUpdateManager.updateIAMBindings(Arrays.asList(
+      PROJECT_2_BINDINGS_DATA_ON_20190620, PROJECT_2_BINDINGS_DATA_ON_20190523));
+    Map<Long, Integer> datesToBindings = dataReadManager
+      .getMapOfDatesToIAMBindings(PROJECT_ID_2);
+
+    assertTrue("The database now contains the dates that were just added",
+      datesToBindings.containsKey(PROJECT_2_BINDINGS_DATA_ON_20190620.getTimestamp()) &&
+      datesToBindings.containsKey(PROJECT_2_BINDINGS_DATA_ON_20190523.getTimestamp()));
+    assertEquals((Integer) PROJECT_2_BINDINGS_DATA_ON_20190620.getBindingsNumber(), 
+      datesToBindings.get(PROJECT_2_BINDINGS_DATA_ON_20190620.getTimestamp()));
+    assertEquals((Integer) PROJECT_2_BINDINGS_DATA_ON_20190523.getBindingsNumber(), 
+      datesToBindings.get(PROJECT_2_BINDINGS_DATA_ON_20190523.getTimestamp()));
+  }
+
+  @Test
+  public void testCAllNewDataDeleted() {
+    dataUpdateManager.deleteYearOldData();
+    Map<Long, Integer> datesToBindings = dataReadManager
+      .getMapOfDatesToIAMBindings(PROJECT_ID_2);
+    Map<Long, Recommendation> datesToRecommendations = dataReadManager
+      .getMapOfDatesToRecommendationTaken(PROJECT_ID_1);
+
+    assertFalse("20190523 is no longer in the bindings database",
+      datesToBindings.containsKey(PROJECT_2_BINDINGS_DATA_ON_20190523.getTimestamp()));
+    assertFalse("20190620 is no longer in the bindings database",
+      datesToBindings.containsKey(PROJECT_2_BINDINGS_DATA_ON_20190620.getTimestamp()));
+    assertFalse("20190625 is no longer in the recommendations database",
+      datesToRecommendations.containsKey(
+        PROJECT_1_RECOMMENDATION_ON_20190625.getAcceptedTimestamp()));
+    assertFalse("20190628 is no longer in the recommendations database",
+      datesToRecommendations.containsKey(
+        PROJECT_1_RECOMMENDATION_ON_20190628.getAcceptedTimestamp()));
+  } 
+
+  @Test
+  public void testDOldDataStillInDatabase() {
+    Map<Long, Integer> datesToBindings = dataReadManager
+      .getMapOfDatesToIAMBindings(PROJECT_ID_2);
+    Map<Long, Recommendation> datesToRecommendations = dataReadManager
+      .getMapOfDatesToRecommendationTaken(PROJECT_ID_1);
+
+    assertTrue("Check for June 12th data in IAM Table", 
+      datesToBindings.containsKey(1591963200000L));
+    assertTrue("Check for June 25th data in IAM Table", 
+      datesToBindings.containsKey(1593086400000L));
+    assertTrue("Check for June 18th in Recommendations Table", 
+      datesToRecommendations.containsKey(1592486705000L));
+    assertTrue("Check for June 8th in Recommendations Table",
+      datesToRecommendations.containsKey(1591633823000L));
+  } 
+}

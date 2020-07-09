06008a8165227a4b8219ffaa9d4522a3746a9e07
Caroline Lui <cel243@cornell.edu>
2020-07-09 16:59:06 +0000

    Create constants

---
+++ src/main/java/com/google/impactdashboard/configuration/Constants.java
@@ -0,0 +1,24 @@
+package com.google.impactdashboard.configuration;
+
+/** A class to hold constants used throughout the project. */
+public class Constants {
+
+  /** The project id of the project being used to deploy this webapp. */
+  public static final String PROJECT_ID = "concord-intern";
+
+  /** The path to the service account key. */
+  public static final String PATH_TO_SERVICE_ACCOUNT_KEY = 
+    "/usr/local/google/home/carolinelui/Downloads/concord-intern-10ce059284ad.json";
+
+  /** The name of the table holding recommendations data. */
+  public static final String RECOMMENDATIONS_TABLE = "Recommendations";
+
+  /** The name of the table holding Iam Bindings data. */
+  public static final String IAM_BINDINGS_TABLE = "IAM_Bindings";
+
+  /** The name of the database holding the test data. */
+  public static final String TEST_DATABASE = "Test_Capstone";
+
+  /** The name of the database holding the real data. */
+  public static final String DATABASE = "";
+}

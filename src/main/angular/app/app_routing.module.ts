43e6e7e2761d0c935151b704fd03395bef95294d
Peter Geertsema <peterjgeertsema@gmail.com>
2020-07-09 18:43:09 +0000

    Improve error page appearance

--- src/main/angular/app/app_routing.module.ts
+++ src/main/angular/app/app_routing.module.ts
@@ -1,7 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {AppComponent} from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'error',
+    component: ErrorPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

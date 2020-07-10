a1efcf4bd7dfa50e2bf3099df102cdc2b78767b8
Peter Geertsema <peterjgeertsema@gmail.com>
2020-07-10 16:04:00 +0000

    Add service for routing to errors

---
+++ src/main/angular/app/services/error_message.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+import {ErrorMessage} from '../../model/error_message';
+import {Router} from '@angular/router';
+import {ERROR_PAGE_URL} from '../../constants';
+
+/** Facilitates the transfer of an error message from one component to the error page and redirrects users to the error page */
+@Injectable()
+export class ErrorMessageService {
+  constructor(private router: Router) {
+    this.errorMessage = [];
+  }
+
+  private errorMessage: ErrorMessage[];
+
+  addError(errors: ErrorMessage[]) {
+    this.router.navigate([`/${ERROR_PAGE_URL}`]);
+    this.errorMessage = errors;
+  }
+
+  getError(): ErrorMessage[] {
+    return this.errorMessage;
+  }
+}

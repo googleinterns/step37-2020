d65384c0f9cd2f73dd30275b3508ded4bad33d5e
Peter Geertsema <peterjgeertsema@gmail.com>
2020-07-17 14:18:46 +0000

    Stub out graph cache tests

--- src/main/angular/app/services/graph_data_cache.service.spec.ts
+++ src/main/angular/app/services/graph_data_cache.service.spec.ts
@@ -1,3 +1,19 @@
 import {GraphDataCacheService} from './graph_data_cache.service';
 
-describe('GraphDataCacheService', () => {});
+describe('GraphDataCacheService', () => {
+  describe('hasEntry()', () => {
+    it('Fails if there is no entry', () => {});
+    it('Fails if entry is out-of-date', () => {});
+    it('Works for an in-date entry', () => {});
+  });
+
+  describe('getEntry()', () => {
+    it('Returns undefined for an empty entry', () => {});
+    it('Returns undefined for an out-of-date entry', () => {});
+    it('Returns a valid cache entry', () => {});
+  });
+
+  describe('addEntry()', () => {
+    it('Adds a new entry successfully', () => {});
+  });
+});

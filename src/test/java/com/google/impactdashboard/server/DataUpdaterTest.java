238944ce4f6776e0d626e3bc954dc691a041990c
Caroline Lui <cel243@cornell.edu>
2020-07-21 17:45:38 +0000

    Add recommendations test

--- src/test/java/com/google/impactdashboard/server/DataUpdaterTest.java
+++ src/test/java/com/google/impactdashboard/server/DataUpdaterTest.java
@@ -1,6 +1,12 @@
 package com.google.impactdashboard.server;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import com.google.apphosting.api.logservice.LogServicePb.LogReadRequest;
+import com.google.logging.v2.LogEntry;
+import com.google.cloud.logging.v2.LoggingClient.ListLogEntriesPagedResponse;
 import com.google.impactdashboard.configuration.Configuration;
 import com.google.impactdashboard.data.project.Project;
 import com.google.impactdashboard.data.project.ProjectGraphData;
@@ -9,6 +15,7 @@ import com.google.impactdashboard.data.project.ProjectMetaData;
 import com.google.impactdashboard.data.recommendation.IAMRecommenderMetadata;
 import com.google.impactdashboard.data.recommendation.Recommendation;
 import com.google.impactdashboard.data.recommendation.RecommendationAction;
+import com.google.impactdashboard.data.recommendation.Recommendation.RecommenderType;
 import com.google.impactdashboard.database_manager.data_read.DataReadManager;
 import com.google.impactdashboard.database_manager.data_read.DataReadManagerFactory;
 import com.google.impactdashboard.database_manager.data_update.DataUpdateManager;
@@ -36,6 +43,37 @@ public class DataUpdaterTest extends Mockito {
   private DataUpdater manualDataUpdater;
   private DataUpdater automaticDataUpdater;
 
+  private static final ProjectIdentification PROJECT_1 = 
+      ProjectIdentification.create("project-1", "project-id-1", 123456789123L);
+  private static final ProjectIdentification PROJECT_2 = 
+      ProjectIdentification.create("project-2", "project-id-2", 234567890123L);
+  private static final ProjectIdentification PROJECT_3 = 
+      ProjectIdentification.create("project-3", "project-id-3", 345678901234L);
+  private static final Recommendation PROJECT_3_RECOMMENDATION_1 = 
+      Recommendation.create("project-id-3", "test@example.com", 
+          Arrays.asList(
+            RecommendationAction.create(
+              "affected@example.com", "role1", "",
+              RecommendationAction.ActionType.REMOVE_ROLE)), 
+          Recommendation.RecommenderType.IAM_BINDING, 1593072412000L, 
+          IAMRecommenderMetadata.create(350));
+  private static final Recommendation PROJECT_3_RECOMMENDATION_2 = 
+      Recommendation.create("project-id-3", "test@example.com", 
+          Arrays.asList(
+            RecommendationAction.create(
+              "affected@example.com", "role1", "role2",
+              RecommendationAction.ActionType.REPLACE_ROLE)), 
+          Recommendation.RecommenderType.IAM_BINDING, 1593070012000L, 
+          IAMRecommenderMetadata.create(400));
+  private static final Recommendation PROJECT_1_RECOMMENDATION = 
+      Recommendation.create("project-id-1", "test@example.com", 
+          Arrays.asList(
+            RecommendationAction.create(
+              "affected@example.com", "role1", "",
+              RecommendationAction.ActionType.REMOVE_ROLE)), 
+          Recommendation.RecommenderType.IAM_BINDING, 1593078012000L, 
+          IAMRecommenderMetadata.create(1000));
+
   @Before
   public void setup() {
     Configuration.useFakeDataReadManager = true;
@@ -58,6 +96,61 @@ public class DataUpdaterTest extends Mockito {
         fakeDataReadManager, mockIamBindingRetriever, mockProjectListRetriever, false);
   }
 
-  // @Test
-  // public void 
+  private void initializeRecommendationFakes() {
+
+  }
+
+  @Test
+  public void testAutomaticUpdateRecommendationsWith1NewProject2Old() {
+    ListLogEntriesPagedResponse project3Response = mock(ListLogEntriesPagedResponse.class);
+    ListLogEntriesPagedResponse project2Response = mock(ListLogEntriesPagedResponse.class);
+    ListLogEntriesPagedResponse project1Response = mock(ListLogEntriesPagedResponse.class);
+    List<LogEntry> project3RecommendationLogs = 
+        Arrays.asList(mock(LogEntry.class), mock(LogEntry.class));
+    List<LogEntry> project1RecommendationLogs = 
+        Arrays.asList(mock(LogEntry.class));
+    List<Recommendation> project3Recommendations = 
+        Arrays.asList(PROJECT_3_RECOMMENDATION_1, PROJECT_3_RECOMMENDATION_2);
+    List<Recommendation> project1Recommendations = 
+        Arrays.asList(PROJECT_1_RECOMMENDATION); 
+    
+    when(mockProjectListRetriever.listResourceManagerProjects())
+        .thenReturn(new ArrayList<>(Arrays.asList(PROJECT_1, PROJECT_2, PROJECT_3)));
+    when(mockLogRetriever.listRecommendationLogs(PROJECT_3.getProjectId(), "", ""))
+        .thenReturn(project3Response);
+    when(mockLogRetriever.listRecommendationLogs(
+            eq(PROJECT_1.getProjectId()), argThat(time -> !time.equals("")), eq("")))
+        .thenReturn(project1Response);
+    when(mockLogRetriever.listRecommendationLogs(
+            eq(PROJECT_2.getProjectId()), argThat(time -> !time.equals("")), eq("")))
+        .thenReturn(project2Response);
+
+    when(project1Response.iterateAll()).thenReturn(project1RecommendationLogs);
+    when(project3Response.iterateAll()).thenReturn(project3RecommendationLogs);
+    when(project2Response.iterateAll()).thenReturn(Arrays.asList());
+
+    when(
+        mockRecommendationRetriever.listRecommendations(
+            project3RecommendationLogs, PROJECT_3.getProjectId(), 
+            Recommendation.RecommenderType.IAM_BINDING, mockIamBindingRetriever))
+        .thenReturn(project3Recommendations);
+    when(
+        mockRecommendationRetriever.listRecommendations(
+            project1RecommendationLogs, PROJECT_1.getProjectId(), 
+            Recommendation.RecommenderType.IAM_BINDING, mockIamBindingRetriever))
+        .thenReturn(project1Recommendations);
+    when(
+        mockRecommendationRetriever.listRecommendations(
+            Arrays.asList(), PROJECT_2.getProjectId(), 
+            Recommendation.RecommenderType.IAM_BINDING, mockIamBindingRetriever))
+        .thenReturn(Arrays.asList());
+
+    List<Recommendation> actual = automaticDataUpdater.listUpdatedRecommendations();
+    List<Recommendation> expected = Arrays.asList(
+        PROJECT_1_RECOMMENDATION, PROJECT_3_RECOMMENDATION_1, PROJECT_3_RECOMMENDATION_2);
+    
+    Assert.assertEquals("Returned lists have the same size", expected.size(), actual.size());
+    actual.removeAll(expected); // if actual is now empty, the lists were equal
+    Assert.assertEquals("Lists are equal", Arrays.asList(), actual);
+  }
 }

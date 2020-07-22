8dfddba819936b8a2362b8d0b48369e26bc63103
cel243 <54676970+cel243@users.noreply.github.com>
2020-07-22 10:06:03 -0400

    Change servlet access link

--- src/main/java/com/google/impactdashboard/servlets/ManualUpdateServlet.java
+++ src/main/java/com/google/impactdashboard/servlets/ManualUpdateServlet.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  * Servlet for handling the scheduled daily update of the database
  * with new IAM data and Recommendation data.
  */
-@WebServlet("/admin")
+@WebServlet("/manual-update")
 public class ManualUpdateServlet extends HttpServlet {
 
   private DataUpdater dataUpdater;
